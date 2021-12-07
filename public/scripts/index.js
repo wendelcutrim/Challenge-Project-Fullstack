86a211ef4116d48c57c2e53db33167618819427a
Wendel Cutrim <wendelcutrim@hotmail.com>
2021-12-07 14:14:34 -0300

    Finish the front-end layout with HTML, CSS, Javascript and EJS.

---
+++ public/scripts/index.js
@@ -0,0 +1,9 @@
+new Glider(document.querySelector('.glider'), {
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    draggable: true,
+    arrows: {
+        prev: '.glider-prev',
+        next: '.glider-next'
+    }
+});
